7c09f325f89ff64de2e1cddd3e9f0cef6a746a2c
Evalac <maslenokprograming.m@gmail.com>
2024-12-12 17:44:22 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactValidation } from './ContactValidation/ContactValidation';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <ContactValidation />
+      </>
+    );
+  }
+}
+
+export { App };
