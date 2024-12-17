6d274a55eb8f96eee9c6c200708d31579bdd2ec9
Evalac <maslenokprograming.m@gmail.com>
2024-12-17 17:34:03 +0200

    add new contact ready? need add filter contact

--- src/components/ContactValidation/ContactValidation.jsx
+++ src/components/ContactValidation/ContactValidation.jsx
@@ -2,21 +2,34 @@ import { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { object, string, email, number, boolean } from 'yup';
 import { nanoid } from 'nanoid';
+import css from './contactValidation.module.css';
 
 class ContactValidation extends Component {
-  // const result = Object.values(initialValue).map((value, index, array) => {
-  //   console.log(value);
-  // });
+  state = {
+    id: '',
+    name: '',
+    number: '',
+  };
+
+  onChange = e => {
+    this.setState({
+      id: nanoid(),
+      [e.currentTarget.name]: e.currentTarget.value, //вичисляємі значення
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
   onSubmit = e => {
-    // e.preventDefault();
-    // в цій функціїї треба зробити логіку на додавання Імя контакту в стейт але не змінюючи його
     console.log('submit');
+    e.preventDefault();
+    this.props.addContact(this.state);
+    this.setState({ id: '', name: '', number: '' });
   };
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label htmlFor="name">
             Name:
             <input
@@ -25,16 +38,33 @@ class ContactValidation extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
+              value={this.state.name}
+              onChange={this.onChange}
             />
           </label>
-          <button type="submit" onClick={e => this.onSubmit(e)}>
-            Add contact
-          </button>
+          <label>
+            Number:
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
+              onChange={this.onChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
         </form>
         <h1>Contact</h1>
         <ul>
-          {Object.values(this.props.initialValue).map((value, index, array) => {
-            return <li key={nanoid()}>{value}</li>;
+          {this.props.initialValue.contacts.map(contact => {
+            return (
+              <li key={nanoid()} className={css.contactItem}>
+                <p>{contact.name}:</p>
+                <p>{contact.number}</p>
+              </li>
+            );
           })}
         </ul>
       </div>
